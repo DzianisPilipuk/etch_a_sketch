0ac3396c280419e8d2b1bd8045311e8acf9787a5
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-03-19 18:58:55 +0100

    Add fixed dimensions of the grid

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-let gridSize = prompt("Select grid size");
+const gridDimensions = 500;
+let gridDensity; 
+getDensity();
+let pixelDimensions = (gridDimensions / gridDensity);
 createGrid();
 
+function getDensity() {
+    do { 
+        gridDensity = prompt("Select grid density, min 16 max 100");
+    } while ( gridDensity < 16 || gridDensity > 100);
+}
 function createGrid() {
-    for (let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < gridDensity; i++) {
         document.getElementById("grid").innerHTML += "<div style='display: flex'>" + createLine() + "</div>";
     }
     function createLine() {
         let line = "";
-        for (let a = 0; a < gridSize; a++) {
-            line += "<div class='pixel' style='width: 20px; height: 20px'></div>";
+        for (let a = 0; a < gridDensity; a++) {
+            line += "<div class='pixel' style='width: " + pixelDimensions + "px; height: " + pixelDimensions + "px'></div>";
         }
         return line;
     }
