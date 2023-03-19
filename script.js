a77d65affd8366292af0a3311eba8770e71f3d64
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-03-19 19:27:41 +0100

    Add grid dimensions responsibility

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const gridDimensions = 500;
+let gridDimensions;
+setGridDimensions();
+function setGridDimensions() {
+    if (window.innerWidth > 550 && window.innerHeight > 550) {
+        gridDimensions = 500;
+    } else if (window.innerWidth < window.innerHeight){
+        gridDimensions = window.innerWidth * 0.9;
+    } else {
+        gridDimensions = window.innerHeight * 0.9;
+    }
+
+}
 let gridDensity; 
 getDensity();
 let pixelDimensions = (gridDimensions / gridDensity);
