3715cdfaf032e3e592f09695c0f5d3313359936a
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-03-21 08:55:46 +0100

    Add pencil/eraser switch

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const slider = document.getElementById("densityRange");
 const gridDensityDisplayer = document.getElementById("gridDensity");
 const grid = document.getElementById("grid");
 const colorPicker = document.getElementById("colorPicker");
+const pencilButton = document.getElementById("pencil");
 const eraserButton = document.getElementById("eraser");
 const clearButton = document.getElementById("clear")
 let paintColor = "#000000";
@@ -22,8 +23,15 @@ gridDensityDisplayer.innerHTML = slider.value;
 colorPicker.oninput = function() {
     paintColor = this.value;
 }
+pencilButton.onclick = function() {
+    paintColor = colorPicker.value;
+    pencilButton.style.setProperty('border-style', 'solid')
+    eraserButton.style.setProperty('border-style', 'none')
+}
 eraserButton.onclick = function() {
     paintColor = "#FFFFFF";
+    eraserButton.style.setProperty('border-style', 'solid')
+    pencilButton.style.setProperty('border-style', 'none')
 }
 clearButton.onclick = function() {
     createGrid();
@@ -47,6 +55,9 @@ function disablePaint() {
     paintEnable = false;
 }
 function createGrid() {
+    paintColor = colorPicker.value;
+    pencilButton.style.setProperty('border-style', 'solid')
+    eraserButton.style.setProperty('border-style', 'none')
     grid.innerHTML = "";
     for (let i = 0; i < gridDensity; i++) {
         grid.innerHTML += "<div style='display: flex'>" + createLine(i) + "</div>";
